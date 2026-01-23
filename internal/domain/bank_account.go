11f8e38bf234e428750b07d4731a2efd8727000b
Rodrigo Thenopholo <thenopholo92@gmail.com>
2026-01-23 10:57:07 -0300

    feat: adicionei as validações báscias de bankAccount

--- internal/domain/bank_account.go
+++ internal/domain/bank_account.go
@@ -7,18 +7,46 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+type AccountType string
+
+const (
+	AccountTypeChecking AccountType = "checking"
+	AccountTypeSavings  AccountType = "savings"
+)
+
+func (a AccountType) IsValid() bool {
+	return a == AccountTypeChecking || a == AccountTypeSavings
+}
+
 type BankAccount struct {
 	ID          uuid.UUID
 	UserID      uuid.UUID
 	Name        string
-	AccountType string
+	AccountType AccountType
 	BankName    string
 	Balance     decimal.Decimal
 	IsActive    bool
 	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
-func NewBankAccount(userID uuid.UUID, accountType, name, bankName string, balance decimal.Decimal, isActive bool) *BankAccount {
+func NewBankAccount(userID uuid.UUID, accountType AccountType, name, bankName string, balance decimal.Decimal) (*BankAccount, error) {
+	if name == "" {
+		return nil, ErrEmptyBankName
+	}
+
+	if name == "" {
+		name = bankName
+	}
+
+	if !accountType.IsValid() {
+		return nil, ErrInvalidAccountType
+	}
+
+	if balance.LessThan(decimal.Zero) {
+		return nil, ErrNegativeBalance
+	}
+
 	return &BankAccount{
 		ID:          uuid.New(),
 		UserID:      userID,
@@ -26,7 +54,7 @@ func NewBankAccount(userID uuid.UUID, accountType, name, bankName string, balanc
 		AccountType: accountType,
 		BankName:    bankName,
 		Balance:     balance,
-		IsActive:    isActive,
+		IsActive:    true,
 		CreatedAt:   time.Now(),
-	}
+	}, nil
 }
